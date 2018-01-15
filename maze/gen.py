3ced933c930f3fe4f923f6a6a03bb1de68c4167c
Jennifer Richards <jennifer@painless-security.com>
2018-01-14 22:31:21 -0500

    Add arguments for print size and margins, and make size 2-dimensional
    Fixes #1 and #2

--- maze/gen.py
+++ maze/gen.py
@@ -4,8 +4,9 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from argparse import ArgumentParser
-import maze
 from pkg_resources import resource_stream
+import papersize
+import maze
 
 
 def plot_maze(ax, mz, start_image=None, end_image=None):
@@ -147,7 +148,7 @@ def plot_maze(ax, mz, start_image=None, end_image=None):
         img_height=start_image.shape[1]
         zoom=50*min(abs(float(tile_width)/img_width),
                  abs(float(tile_height)/img_height))
-        print img_width, zoom
+        #print img_width, zoom
         ax.add_artist(AnnotationBbox(OffsetImage(start_image,
                                                  zoom=zoom),
                                      (tile_width*0.5, tile_height*0.5),
@@ -157,7 +158,7 @@ def plot_maze(ax, mz, start_image=None, end_image=None):
         img_height=end_image.shape[1]
         zoom=50*min(abs(float(tile_width)/img_width),
                  abs(float(tile_height)/img_height))
-        print img_width, zoom
+        #print img_width, zoom
         ax.add_artist(AnnotationBbox(OffsetImage(end_image,
                                                  zoom=zoom),
                                      (surf_width-tile_width*0.5, surf_height-tile_height*0.5),
@@ -165,13 +166,117 @@ def plot_maze(ax, mz, start_image=None, end_image=None):
 #    ax.text(surf_width-0.5*tile_width, surf_height-0.5*tile_height, "End",
 #            ha='center', va='center', size='smaller')
 
+def parse_maze_size(s):
+    """Parse a maze size string into a tuple
+
+    Takes a string of either "N" or "NxM" and converts it into a 2-element
+    tuple with integer elements indicating (width, height).
+
+    :param s: string to parse
+    :return: 2-element tuple, (width, height)
+    """
+    dims = s.lower().split("x")
+    if len(dims) == 1:
+        w = dims[0]
+        h = w
+    elif len(dims) == 2:
+        (w, h) = dims
+    else:
+        raise ValueError("Expected string like 'n' or 'n x m'")
+
+    return (int(w), int(h))
+
+def parse_paper_size(s):
+    """Parse a paper size into a tuple
+
+    Parses a paper size or description (like 'letter') and returns a tuple
+    of (width, height) in inches.
+
+    :param s: string to parse
+    :return: 2-element tuple, (width, height) in inches
+    """
+    (w,h) = papersize.parse_papersize(s, 'in')
+    return map(float, [w, h]) # instead of Decimal
+
+def parse_margin(s):
+    """Parse a margin specification
+    :param s: string to parse
+    :return: 4-element tuple, with (left, right, top, bottom) margins in inches
+    """
+    margins = s.split(',')
+    if len(margins) == 1:
+        left = papersize.parse_length(margins[0], 'in')
+        right = left
+        top = left
+        bottom = left
+    elif len(margins) == 2:
+        left = papersize.parse_length(margins[0], 'in')
+        right = left
+        top = papersize.parse_length(margins[1], 'in')
+        bottom = top
+    elif len(margins) == 4:
+        left = papersize.parse_length(margins[0], 'in')
+        right = papersize.parse_length(margins[1], 'in')
+        top = papersize.parse_length(margins[2], 'in')
+        bottom = papersize.parse_length(margins[3], 'in')
+    else:
+        raise ValueError("Expected 1, 2, or 4 margin specifications")
+
+    return map(float, (left, right, top, bottom))
+
+def margins_to_scale(left, right, top, bottom, width, height):
+    """Convert linear margins to matplotlib axis scale
+
+    Units are arbitrary, but must be the same for all input values
+
+    :param left: left margin
+    :param right: right margin
+    :param top: top margin
+    :param bottom: bottom margin
+    :param width: width of the figure
+    :param height: height of the figure
+    :return: Scale tuple for use with add_axis()
+    """
+    return [float(left)/width,
+            float(bottom) / height,
+            float(width-left-right)/width,
+            float(height - top - bottom) / height]
+
 def main():
     parser = ArgumentParser(description="Randomly generate a maze.")
-    parser.add_argument("--size", "-s", type=int, dest="maze_size", default=20)
+    parser.add_argument("--maze-size", "-s",
+                        type=parse_maze_size,
+                        dest="maze_size",
+                        default="15x15",
+                        help="Number of tiles in each direction as 'width x height' "
+                             "Default = %(default)s")
+    parser.add_argument("--paper-size", "-p",
+                        type=parse_paper_size,
+                        dest="paper_size",
+                        default="letter",
+                        help="Paper size for output (e.g., '8.5in x 11in', 'A4'). "
+                             "If dimensions are given, units default to points "
+                             "if not specified. Default = %(default)s")
+    parser.add_argument("--margin", "-m",
+                        type=parse_margin,
+                        dest="margins",
+                        default="0.5in",
+                        help="Margin to leave around the maze, either as a single length "
+                             "to apply on all sides, 'h,v' for distinct horizontal and vertical "
+                             "margins, or 'l,r,t,b' for distinct left, right, top, and bottom "
+                             "margins. Default = %(default)s")
     parser.add_argument("--inertia", "-i", type=int, dest="inertia", default=0)
-    parser.add_argument("--num", "-n", type=int, dest="num", default=1)
+    parser.add_argument("--num", "-n",
+                        type=int,
+                        dest="num",
+                        default=1,
+                        help="Number of mazes to generate. Output will be a PDF with one "
+                             "maze per page. Default = %(default)s")
     parser.add_argument("--seed", type=int, dest="seed", default=None)
     args=parser.parse_args()
+
+    print args
+    print margins_to_scale(*(args.margins + args.paper_size))
 
     if args.seed is not None:
         import random
@@ -180,11 +285,13 @@ def main():
     plt.xkcd()
     pp=PdfPages('maze.pdf')
     for nn in range(args.num):
-        mz = maze.Maze(args.maze_size, args.maze_size)
+        mz = maze.Maze(*args.maze_size)
         mz.generate(args.inertia)
 
-        fig = plt.figure(figsize=(6,6), dpi=300)
-        ax = fig.add_axes((0.02,0.02,0.96,0.96), aspect='equal', frameon=False)
+        fig = plt.figure(figsize=args.paper_size, dpi=300)
+        ax = fig.add_axes(
+            margins_to_scale(*(args.margins + args.paper_size)),
+            frameon=False)
 
         plot_maze(ax, mz,
                   plt.imread(resource_stream('maze.resources.images', 'smiley.png')),
